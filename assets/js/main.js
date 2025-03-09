1467bc923d0de5117604f9f9b8589790365d6f33
mehdip2007 <mehdipourhadi@hotmail.com>
2025-03-09 12:56:07 +0330

    add carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
-// Function to toggle the visibility of the right navbar
-function toggleMenu() {
-    const navbarRight = document.querySelector('.navbar-right');
-    if (navbarRight) {
-        navbarRight.classList.toggle('active');
-    } else {
-        console.error("Element '.navbar-right' not found.");
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector("#productCarousel");
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    if (carousel) {
+        carousel.addEventListener("touchstart", function(e) {
+            touchStartX = e.changedTouches[0].screenX;
+        });
+
+        carousel.addEventListener("touchend", function(e) {
+            touchEndX = e.changedTouches[0].screenX;
+            if (touchStartX - touchEndX > 50) {
+                document.querySelector("#productCarousel .carousel-control-next").click();
+            } else if (touchEndX - touchStartX > 50) {
+                document.querySelector("#productCarousel .carousel-control-prev").click();
+            }
+        });
     }
-}
+});
